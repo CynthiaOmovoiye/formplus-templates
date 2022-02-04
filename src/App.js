989dc156a5b9a6eb8f7e15bf981f8cdda45254a1
cynthia.omovoiye <omovoiye@gmail.com>
2022-02-04 11:41:46 +0100

    update on Header and Loader components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import Template from "./components/Template"
 import Label from "./components/Label"
+import Header from "./components/Header"
+import Loader from './components/Loader';
 
-
-let PageSize = 12;
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [templates, setTemplates] = useState([]);
-  let   [categoriesTemplates, setCategoriesTemplates] = useState([]);
+  const [allTemplates, setAllTemplates] = useState([]);
+  const [templates, setTemplates] = useState(allTemplates);
+  let [categoriesTemplates, setCategoriesTemplates] = useState(allTemplates);
   const [isLoading, setIsLoading] = useState(false);
   const [filteredCategory, setFilteredCategory] = useState("All");
-  const [selectedName, setSelectedName] = useState("");
-  const [selectedDate, setSelectedDate] = useState("");
-  const [searchValue, setSearchValue] = useState("")
-
+  let PageSize = 12;
 
   useEffect(() => {
     setIsLoading(true);
@@ -25,58 +23,17 @@ function App() {
   }, []);
 
   useEffect(() => {
-    handleSearch(searchValue)
-}, [searchValue])
+    setTemplates(categoriesTemplates)
+  }, [categoriesTemplates])
 
   const fetchtemplates = async () => {
     const response = await axios(
       `https://front-end-task-dot-result-analytics-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates`
     );
+    setAllTemplates(response.data)
     setTemplates(response.data)
     setCategoriesTemplates(response.data)
     setIsLoading(false);
-  };
- 
-  const FilterByCategory = (value) => {
-    setFilteredCategory(value)
-    setSelectedName("");
-    setSelectedDate("")
-    setSearchValue("")
-    if (value !== "All") {
-      categoriesTemplates = categoriesTemplates.filter((_data) => _data?.category?.includes(value));
-    }
-    return setTemplates(categoriesTemplates);
-  };
-
-  const handleSearch = (value) => {
-    if(value != ""){
-     categoriesTemplates = categoriesTemplates.filter((_data) => _data?.name?.toLowerCase().includes(value?.toLowerCase()));
-    }
-    return setTemplates(categoriesTemplates); 
-};
-
-  const FilterByDate = (value) => {
-    setSelectedDate(value)
-    setSelectedName("");
-    if(value == "Ascending"){
-      categoriesTemplates = [...categoriesTemplates].sort((dateA, dateB) => dateA.created.localeCompare(dateB.created))
-     }
-     else if(value == "Descending"){
-      categoriesTemplates = [...categoriesTemplates].sort((dateA, dateB) => dateA.created.localeCompare(dateB.created)).reverse()
-     }
-     return setTemplates(categoriesTemplates);
-   
-  }
-  const FilterByAlphabetically = (value) => {
-    setSelectedName(value)
-    setSelectedDate("")
-    if(value == "Ascending"){
-      categoriesTemplates = [...categoriesTemplates].sort((dateA, dateB) => dateA.name.localeCompare(dateB.name))
-     }
-     else if(value == "Descending"){
-      categoriesTemplates = [...categoriesTemplates].sort((dateA, dateB) => dateA.name.localeCompare(dateB.name)).reverse()
-     }
-     return setTemplates(categoriesTemplates);
   };
 
   const currentTemplateData = useMemo(() => {
@@ -88,89 +45,26 @@ function App() {
   return (
 
     <div className="container mx-auto p-10">
-      <div className='flex mb-5 justify-between flex-wrap items-center mb-3'>
-        <div className='w-full md:w-1/4 flex justify-center md:justify-start'>
-          <input
-            type="text"
-            name="first-name"
-            id="first-name"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder='Search Templates'
-            className="shadow-sm w-full sm:text-sm border border-gray-100 rounded-md p-2"
-          />
-        </div>
-
-        <div className='flex sm:flex-row flex-col justify-around items-center w-full md:w-3/5'>
-          <span>Sort by:</span>
-          <div className='w-full sm:w-1/4 '>
-            <label htmlFor="category" className="block text-sm font-medium text-gray-700">
-              Category
-            </label>
-            <select
-              id="category"
-              name="category"
-              onChange={(e) => FilterByCategory(e.target.value)}
-              className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm  sm:text-sm"
-            >
-              <option value="All">All</option>
-              <option value="Health">Health</option>
-              <option value="E-commerce">E-commerce</option>
-              <option value="Education">Education</option>
-            </select>
-          </div>
-          <div className='w-full sm:w-1/4 '>
-            <label htmlFor="order" className="block text-sm font-medium text-gray-700">
-              Order
-            </label>
-            <select
-              id="order"
-              name="order"
-              value={selectedName}
-              onChange={(e) => FilterByAlphabetically(e.target.value)}
-              className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm sm:text-sm"
-            >
-              <option value=''>Default</option>
-              <option value='Ascending'>Ascending</option>
-              <option value='Descending'>Descending</option>
-            </select>
-          </div>
-
-          <div className='w-full sm:w-1/4 '>
-            <label htmlFor="date" className="block text-sm font-medium text-gray-700">
-              Date
-            </label>
-            <select
-              id="date"
-              name="date"
-              value={selectedDate}
-              onChange={(e) => FilterByDate(e.target.value)}
-              className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm sm:text-sm"
-            >
-              <option value=''>Default</option>
-              <option value='Ascending'>Ascending</option>
-              <option value='Descending'>Descending</option>
-            </select>
-          </div>
-
-
-        </div>
-      </div>
-      <Label/>
-     
+      <Header
+        allTemplates={allTemplates}
+        setCategoriesTemplates={setCategoriesTemplates}
+        setFilteredCategory={setFilteredCategory}
+        setTemplates={setTemplates}
+        categoriesTemplates={categoriesTemplates}
+      />
+      <Label />
       {isLoading ? (
-        <p className="loading">Loading Templates...</p>
+       <Loader/>
       ) : (
-        
         <>
-        <p className="mb-3">{filteredCategory} Templates</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-2">
-          {currentTemplateData.length ? (currentTemplateData.map((template, index) => (
-            <Template key = {index} template={template}/>
-          ))):(<>
-          <div className="font-bold text-xl mb-2 text-neutral-100 flex items-center justify-center">No Result</div>
-          </>)}
-        </div>
+          <p className="mb-3">{filteredCategory} Templates</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-2">
+            {currentTemplateData.length ? (currentTemplateData.map((template, index) => (
+              <Template key={index} template={template} />
+            ))) : (<>
+              <div className="font-bold text-xl mb-2 text-neutral-100 flex items-center justify-center">No Result</div>
+            </>)}
+          </div>
           <Pagination
             className="pagination-bar"
             currentPage={currentPage}
